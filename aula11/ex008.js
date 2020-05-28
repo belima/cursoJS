14f0c16a05457164d80528499bed51a2307bef97
Bê Lima <lima.bernardo@gmail.com>
2020-05-28 18:04:12 -0300

    aula 11 finalizada

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velo = 78.2
+console.log(`A velocidade é de ${velo} km/h`)
+
+if (velo > 60) {
+    console.log('Você ultrapassou a velcidade permitida. MULTADO')
+}
+console.log('Use sempre o cinto de segurança!')
